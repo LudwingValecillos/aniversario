a2d8e008d06fc63ecc30e9733b652138e1a2f82f
LudwingValecillos <ludwingvaldev@gmail.com>
2024-12-27 17:29:41 -0300

    a

--- components/MessageWall.tsx
+++ components/MessageWall.tsx
@@ -17,7 +17,7 @@ export default function MessageWall(): React.JSX.Element {
 
   return (
     <section className="py-10 px-4">
-      <h2 className="text-3xl font-semibold mb-8 text-center text-red-600">Muro de Mensajes</h2>
+      <h2 className="text-3xl font-semibold mb-8 text-center text-red-600">Mensajitos</h2>
       <form onSubmit={handleSubmit} className="mb-6">
         <input
           type="text"
@@ -30,7 +30,7 @@ export default function MessageWall(): React.JSX.Element {
           type="submit"
           className="mt-3 w-full bg-red-500 text-white py-3 rounded-full text-lg font-semibold hover:bg-red-600 transition duration-300 shadow-lg"
         >
-          Enviar mensaje
+          Enviar uwu
         </button>
       </form>
       <div className="space-y-4">
