96e7b5ce5ca6e4c68996c53c203884055929abe7
LudwingValecillos <ludwingvaldev@gmail.com>
2024-12-27 17:02:32 -0300

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Enable static export
+  distDir: 'dist', // Specify the distribution directory
+  images: {
+    unoptimized: true // Required for static export when using Image component
+  }
+};
 
 export default nextConfig;
